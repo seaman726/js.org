ee9fbd66344788f390dc6f7c791e32a7d87492ef
seaman726 <1164426971@qq.com>
2024-05-22 17:47:36 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3574,7 +3574,8 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "seaman726": "seaman726.github.io/seaman726"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
